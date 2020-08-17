10922a4b8fc0fde5dde15c8cef3195946872c38b
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2020-08-17 19:39:11 +0200

    update: clean up comments

--- src/auth/tasksAPISettings.js
+++ src/auth/tasksAPISettings.js
@@ -1,10 +1,6 @@
 const tasksAPIUrl = "https://jarzebak.eu/dawid/tasks";
 
 const httpHeader = {
-  // crossDomain: true,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
   auth: {
     username: "dawid",
     password: "WAFmkpSI",
