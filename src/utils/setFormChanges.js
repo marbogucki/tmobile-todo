1bcc06ed026a650c26cba1741cd465ca9444622b
Marcin Bogucki <mar.bogucki@gmail.com>
2020-09-08 22:41:15 +0200

    Update setFormChanges.js

--- src/utils/setFormChanges.js
+++ src/utils/setFormChanges.js
@@ -1,15 +1,24 @@
 const setFormChanges = (event, setFormState, formState) => {
-  const {
-    target: { name },
-  } = event;
-  const value =
-    event.target.type === "checkbox"
-      ? event.target.checked
-      : event.target.value;
+//   const {
+//     target: { name },
+//   } = event;
+  const { name, type, checked, value } = event.target;
+
+  const val = type === "checkbox" ? checked : value;
+
   setFormState({
-    ...formState,
-    [name]: value,
+      ...formState,
+      [name]: val
   });
+  
+//   const value =
+//     event.target.type === "checkbox"
+//       ? event.target.checked
+//       : event.target.value;
+//   setFormState({
+//     ...formState,
+//     [name]: value,
+//   });
 };
 
 export default setFormChanges;
